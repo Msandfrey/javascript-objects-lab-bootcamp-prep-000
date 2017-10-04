ebe191ffab8f701fafeb06cb35631cef5e46ad90
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 21:11:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var recipes = {}
 function updateObjectWithKeyAndValue(obj, key, val){
-  return Object.assign(obj, {key: val})
+  return Object.assign({}, obj, {key: val})
 }
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, val){
   obj[key] = val
   return obj
+}
+function deleteFromObjectByKey(obj, key){
+  var object = obj
+  delete object[key]
+  return object
 }
